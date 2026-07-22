082b035a1a2f1792af114729d74f65623f0287d6
Shivangi chaubey <118792511+shvangi-6@users.noreply.github.com>
2026-07-22 16:13:15 +0530

    Add script to build and push Docker image

---
+++ nginx/nginx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TAG=$1  # Capture the first argument passed to the script
+tag=v1.$TAG
+registry=tiwari123
+repository=3-tier-nginx
+
+# Build the Docker image
+docker build -t $registry/$repository:$tag .
+
+# Push the Docker image to the registry
+docker push $registry/$repository:$tag
